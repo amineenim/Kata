24df7726a4db23facb8d8871d85d928ffc62f036
amineenim <aminemaourid1995@gmail.com>
2023-10-16 03:13:25 +0200

    defining prometheus for service games and adding the deploy kube file

--- games/games/src/main/java/com/example/games/GamesApplication.java
+++ games/games/src/main/java/com/example/games/GamesApplication.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableCircuitBreaker
 @EnableHystrixDashboard
 @EnableDiscoveryClient
