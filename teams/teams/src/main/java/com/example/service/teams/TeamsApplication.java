283f03b9cf917b5e8c78cbacc353b321f7d2ceaa
amineenim <aminemaourid1995@gmail.com>
2023-10-16 03:06:24 +0200

    adding prometheus.yml for teamsService and the deploy kube file

--- teams/teams/src/main/java/com/example/service/teams/TeamsApplication.java
+++ teams/teams/src/main/java/com/example/service/teams/TeamsApplication.java
@@ -3,6 +3,7 @@ package com.example.service.teams;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableEurekaClient
 @EnableCircuitBreaker
 @EnableHystrixDashboard
+@EnableDiscoveryClient
 public class TeamsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TeamsApplication.class, args);
---
+++ teams/teams/src/main/java/com/example/service/teams/configuration/HystrixDashboardConfig.java
@@ -0,0 +1,14 @@
+package com.example.service.teams.configuration;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HystrixDashboardConfig {
+    @Bean
+    public ServletRegistrationBean hystrixStreamServlet() {
+        return new ServletRegistrationBean(new HystrixMetricsStreamServlet(), "/hystrix.stream");
+    }
+}
